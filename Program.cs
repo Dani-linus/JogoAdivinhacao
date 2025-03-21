69b207a920b9fd775115e36a57ba99a37285a292
Dani-linus <dns40625@gmail.com>
2025-03-20 21:26:45 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace JogoAdivinhacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Versão 1: Estrutura básica e entrada do usuário
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Jogo de Adivinhação");
+            Console.WriteLine("---------------------------");
+
+            Console.Write("Digite um número: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Você digitou o seguinte número: " + numero);
+            Console.ReadLine();
+        }
+    }
+}
