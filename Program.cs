c02281a0f1c786ad4436d1b33b595647a3b1a8d0
Dani-linus <dns40625@gmail.com>
2025-03-20 22:11:57 -0300

    Versão 3: Verificar se o jogador acertou

--- Program.cs
+++ Program.cs
@@ -6,22 +6,38 @@ namespace JogoAdivinhacao
     {
         static void Main(string[] args)
         {
-            //Versão 2: Gerar um número secreto aleatório
+            //Versão 3: Verificar se o jogador acertou
             Console.WriteLine("---------------------------");
             Console.WriteLine("Jogo de Adivinhação");
             Console.WriteLine("---------------------------");
 
             Random geradornumero = new Random();
-            int numeroaleatorio = geradornumero.Next(1, 11);
+            int numeroaleatorio = geradornumero.Next(1, 5);
 
             Console.Write("Digite um número entre 1 a 10: ");
             int numero = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Você digitou o seguinte número: " + numero);
-            Console.WriteLine($"O número secreto é: {numeroaleatorio}");
-            Console.ReadLine();
+           
 
-            
+            if (numero == numeroaleatorio)
+            {
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("Parabens, você acertou o número secreto: ");
+                Console.WriteLine("---------------------------");
+                
+            }
+            else if (numero < numeroaleatorio)
+            {
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("O número digitado é menor que o número aleatório");
+                Console.WriteLine("---------------------------");
+            }
+            else
+            {
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("O número digitado é maior que o número aleatório");
+                Console.WriteLine("---------------------------");
+            }
+            }
         }
-    }
 }
